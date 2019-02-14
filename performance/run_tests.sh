6797918edbd4727fe604be6884eda371e0ba08b1
Dawid Figiel <dawid.figiel@grandparade.co.uk>
2019-02-14 15:19:38 +0100

    Improve run_test script for load tests

--- performance/run_tests.sh
+++ performance/run_tests.sh
@@ -3,5 +3,14 @@
 echo "Open browser and go to: http://127.0.0.1:8089/"
 
 #locust -f locust_tests.py --host=http://localhost:8080
+#locust -f locust_tests.py --host=http://curexchange-env.rtzvznrmps.eu-west-1.elasticbeanstalk.com
 
-locust -f locust_tests.py --host=http://curexchange-env.rtzvznrmps.eu-west-1.elasticbeanstalk.com
+HOST=$1
+
+if [ "$HOST" = "" ]; then
+   HOST=http://localhost:8080
+fi
+
+echo "locust -f locust_tests.py --host=$HOST"
+
+locust -f locust_tests.py --host=$HOST
